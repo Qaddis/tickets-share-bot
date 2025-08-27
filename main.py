dbdc29cfa93a6f8cf7ed42de422a67fcd06c79ad
Qaddis <svyatoslavb1107@gmail.com>
2025-08-28 01:01:01 +0300

    Initial setup completed

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+from config import TOKEN
+
+bot = Bot(
+    token=TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def cmd_start(msg: Message):
+    await msg.answer(f"<b>Привет</b>, {msg.from_user.id}!")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
 if __name__ == "__main__":
-    print("Hello world!")
+    asyncio.run(main())
