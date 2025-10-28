4ff890c35e3fd1c7f045858d05edab552ad22030
Roma Baranov <romab9386@gmail.com>
2025-10-28 23:54:19 +0300

    Working on the /get command: start work, add first router.

--- app/handlers/get.py
+++ app/handlers/get.py
@@ -1,9 +1,17 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+
+from app.fsm.get import TicketsToGet
+from app.functions.get import get_subject
 
 router = Router()
 
 @router.message(Command("get"))
-async def cmd_get(msg:Message):
-      pass
+async def cmd_get(msg: Message, state: FSMContext):
+    await state.clear()
+      
+    await msg.answer("Начинаем поиск нужных вам билетов. Введите курс")
+      
+    await state.set_state(TicketsToGet.course)
